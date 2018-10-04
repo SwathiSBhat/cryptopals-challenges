327177769c71812d484e87b528a8f246c167d225
swathi s bhat <swathibhat282@gmail.com>
2018-10-04 14:34:56 +0530

    Update chall_2_fixed-xor.py

--- chall_2_fixed-xor.py
+++ chall_2_fixed-xor.py
@@ -1,4 +1,12 @@
-str1=raw_input("enter str1\n")
-str2=raw_input("enter str2\n")
-if(len(str1)==len(str2)):
-	print hex(int(str1,16) ^ int(str2,16))
+from __future__ import print_function
+
+def fixed_xor(s1, s2):
+	if len(s1) == len(s2):
+		return hex(int(s1, 16) ^ int(s2, 16))
+	else:
+		raise ValueError("Strings not of equal length!")
+
+if __name__ == "__main__":
+	str1=raw_input("enter hex str1\n")
+	str2=raw_input("enter hex str2\n")
+	print("Fixed xor of {} and {} = {}".format(str1,str2,fixed_xor(str1,str2)))
